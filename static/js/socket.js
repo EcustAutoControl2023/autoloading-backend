01b85bc5708a8c01abaf2537aac690078cdac82f
Chen Jiyang <masarity@outlook.com>
2023-10-20 17:19:50 +0800

    ✨ feat(UI): 车辆数据显示
    - jQuery + sokect实时数据显示
    - 激光雷达滤波程序（参考

---
+++ static/js/socket.js
@@ -0,0 +1,29 @@
+var traffic_socket = io('http://localhost:5000');
+
+traffic_socket.on('traffic_data', function() {
+    console.log('Connected to server');
+});
+
+traffic_socket.on('traffic_data', function(data) {
+    // 清空表格
+    // $('#car-data').empty();
+
+    // 遍历数据(数据是object，添加到表格中
+    console.log(data);
+    var row = $('<tr>');
+    row.append($('<td style ="color: white" align="center">').text(data['truckid']));
+    row.append($('<td style ="color: white" align="center">').text(data['truckweightin']));
+    row.append($('<td style ="color: white" align="center">').text(data['truckweightout']));
+    row.append($('<td style ="color: white" align="center">').text(data['goodstype']));
+    row.append($('<td style ="color: white" align="center">').text(data['truckload']));
+    row.append($('<td style ="color: white" align="center">').text(data['loadcurrent']));
+    row.append($('<td style ="color: white" align="center">').text(data['storeid']));
+    row.append($('<td style ="color: white" align="center">').text(data['loaderid']));
+    $('#car-data').append(row);
+    // $.each(data, function(key, value) {
+        // console.log(key);
+        // console.log(value);
+    //     $('#car-data').append('<tr><td>' + value['id'] + '</td><td>' + value['time'] + '</td><td>' + value['speed'] + '</td><td>' + value['location'] + '</td><td>' + value['status'] + '</td></tr>');
+    // }
+    // );
+});
