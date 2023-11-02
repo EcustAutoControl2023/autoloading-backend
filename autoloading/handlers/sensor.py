ffa96d3761f5a2ec62bf33905f623c72dcc9f73f
Chen Jiyang <masarity@outlook.com>
2023-11-02 12:07:23 +0800

    🐞 fix(物位计): 断连问题

--- autoloading/handlers/sensor.py
+++ autoloading/handlers/sensor.py
@@ -10,7 +10,8 @@ server_ip=('192.168.100.8',8234)#相机的ip地址、端口号
 hex_data='010320010001DE0A'#发送给物位计的命令
 byte_data = bytes.fromhex(hex_data)
 # s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)# TCP
-# s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)# UDP
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)# UDP
+s.setblocking(False)
 running = True
 int_distance = 0
 timer = None
@@ -24,13 +25,17 @@ def read_per_second():#每秒读取一次物位计数据
     global running
     from .socket import sensor_data
 
+    received_data = -1
 
     while running:  
         # 发送modbus指令，接受数据
-        s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)# UDP
-        s.connect(server_ip)
-        s.sendto(byte_data)
-        received_data = s.recvfrom()
+        s.sendto(byte_data, server_ip)
+        while True:
+            try:
+                received_data,addr = s.recvfrom(1024)
+            except socket.error as e:
+                continue
+            break
         hex_received_data = received_data.hex()  
         current_time = datetime.datetime.now()
         hex_distance = hex_received_data[6:10]
@@ -40,17 +45,17 @@ def read_per_second():#每秒读取一次物位计数据
             insert_data(int_distance,current_time)
         
         # 发送物位计数据到前端
-            sensor_data({
-                'value': int_distance
-            })
-            time.sleep(1)
-        s.close()
+        sensor_data({
+            'value': int_distance
+        })
+        time.sleep(1)
+    s.close()
 
 # 将测量值和时间存储在数据库中
 def insert_data(int_distance,current_time):
     # logging.debug('insert data')
     # XXX:滤波，至少5个数据（线性变化，
-    if int_distance < 3500: # 确保存入有效数据
+    if int_distance < 3000: # 确保存入有效数据
         sensor = Sensor(data=int_distance,time=current_time)
         db.session.add(sensor)
         db.session.commit()
