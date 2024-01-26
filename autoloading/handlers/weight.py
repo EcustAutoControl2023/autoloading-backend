6fc3c500bca52e0f538e617ebc2bede7ef181a6f
Chen Jiyang <masarity@outlook.com>
2024-01-26 21:07:55 +0800

    fix(后端): 估计重量
    - 列表改循环

--- autoloading/handlers/weight.py
+++ autoloading/handlers/weight.py
@@ -6,17 +6,19 @@ def weight_estimate(goods_type,loader_id,time_difference):
 
     per_second_weight = 0
     #筛选数据
-    filtered_traffic = Traffic.query.filter(and_(Traffic.loaderid=='loader_id',Traffic.goodstype=='goods_type')).all()
+    filtered_traffic = Traffic.query.filter(and_(Traffic.loaderid==loader_id,Traffic.goodstype==goods_type)).all()
     
     filtered_data_length = len(filtered_traffic)
     logging.debug(f'filtered_data_length{filtered_data_length}')
       
     #提取筛选后数据的某一属性
-    start_load_time = [traffic.loadertime1 for traffic in filtered_traffic]
-    stop_load_time = [traffic.loadertime2 for traffic in filtered_traffic]
+    start_load_times = [traffic.loadertime1 for traffic in filtered_traffic]
+    stop_load_times = [traffic.loadertime2 for traffic in filtered_traffic]
     loader_task = [traffic.loadertime2 for traffic in filtered_traffic]
     #计算装载时间
-    load_time = [(start_load_time - stop_load_time).total_seconds()]
+    # load_time = [(start_load_time - stop_load_time).total_seconds()]
+    load_time = [(start_load_time - stop_load_time).total_seconds() for \
+                 start_load_time, stop_load_time in zip(start_load_times, stop_load_times)]
     logging.debug(f'load_time{load_time}')
 
     for i in range(filtered_data_length): 
